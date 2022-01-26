6dd32ddd3f105f1595ffc58a5dc1fce947f568c8
SoTrx <-->
2022-01-26 20:16:07 +0100

    Removing keyvault set in script due to wrong rights

--- deploy.ps1
+++ deploy.ps1
@@ -36,16 +36,17 @@ logToStdout [ColorMap]::Debug "Creating infrastructure"
 #deployTerraform
 logToStdout [ColorMap]::Debug "Infrastructure created"
 
-logToStdout [ColorMap]::Debug "Storing admin pseudo-secret in keyvault"
-az keyvault secret set --name "adminOnly" --vault-name $KV_NAME --value "SECRET STRING"
-logToStdout [ColorMap]::Debug "Secret stored"
+#logToStdout [ColorMap]::Debug "Storing admin pseudo-secret in keyvault"
+#az keyvault secret set --name "adminOnly" --vault-name $KV_NAME --value "SECRET STRING"
+#logToStdout [ColorMap]::Debug "Secret stored"
 
 
 # Setting up Istio and cluster DNS link
 logToStdout [ColorMap]::Debug "Fetching deployments secrets"
 $swaApiKey = az staticwebapp secrets list -g $RG_NAME -n $SWA_NAME --query properties.apiKey
 $functionAppApiKey = az functionapp deployment list-publishing-profiles -g $RG_NAME -n $FA_NAME --xml
+$sanitizedSwaApiKey = $swaApiKey -Replace '"',''
 logToStdout [ColorMap]::Debug "Deployments secrets retrieved"
 logToStdout [ColorMap]::Info "IMPORTANT : Please add the following secrets to your repository. You can then push your fork to github to deploy"
-logToStdout [ColorMap]::Info "AZURE_STATIC_WEB_APPS_API_TOKEN : $swaApiKey"
+logToStdout [ColorMap]::Info "AZURE_STATIC_WEB_APPS_API_TOKEN : $sanitizedSwaApiKey"
 logToStdout [ColorMap]::Info "AZURE_FUNCTIONAPP_PUBLISH_PROFILE : $functionAppApiKey"
